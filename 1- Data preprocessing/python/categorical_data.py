289bece05a98eff891a8a4407b39e0210c07dcca
Zoe Duan <30881036+zoeduan@users.noreply.github.com>
2022-11-04 08:27:31 -0500

    Update categorical_data.py

--- 1- Data preprocessing/python/categorical_data.py
+++ 1- Data preprocessing/python/categorical_data.py
@@ -28,3 +28,31 @@ X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+
+```
+The comma inside the bricks seperates the rows from the columns you want to slide from your array.
+
+x[row,column]
+You can place ":" before or after the row and column values. Before the value it means "unitl" and after the value it means "from".
+
+For example you have:
+
+x: array([[5.1, 3.5, 1.4, 0.2],
+          [4.9, 3. , 1.4, 0.2],
+          [4.7, 3.2, 1.3, 0.2],
+          [4.6, 3.1, 1.5, 0.2],
+          [5. , 3.6, 1.4, 0.2],
+          [5.4, 3.9, 1.7, 0.4],
+          [4.6, 3.4, 1.4, 0.3],
+          [5. , 3.4, 1.5, 0.2],
+          [4.4, 2.9, 1.4, 0.2]])
+
+x[:,:] would mean u want every row and every column.
+
+x[3,3] would mean u want the 3 row and the 3 column value
+
+x[:3,:3] would mean u want the rows and columns until 3
+
+x[:, 3] would mean u want the 3 column and every row
+```
